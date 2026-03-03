da31bbb32ae9ef7149a38ba563c4f75a7ada508c
Abel Tesfa <abeltesfa198@gmail.com>
2026-03-03 11:55:03 +0300

    feat: Risk checker service added

--- backend/src/services/graph_service.rs
+++ backend/src/services/graph_service.rs
@@ -1,8 +1,9 @@
+use crate::models::{DrugInteraction, PatientMedication};
 use mork_rust_sdk::{
-    MorkApiClient, UploadRequest, TransfromRequest, ReadRequest, Transform Request, Mm2Cell, Namespace
+    Mm2Cell, MorkApiClient, Namespace, ReadRequest, TransformDetails, TransformRequest,
+    UploadRequest,
 };
 use std::path::PathBuf;
-use crate::models::{DrugInteraction, PatientMedication};
 
 pub struct GraphService {
     client: MorkApiClient,
@@ -11,17 +12,23 @@ pub struct GraphService {
 impl GraphService {
     pub fn new() -> Self {
         GraphService {
-            client: MorkApiClient::new()
+            client: MorkApiClient::new(),
         }
     }
 
     pub async fn ingest_interaction(&self, data: DrugInteraction) -> Result<String, String> {
-        let interaction_fact = format!("(interacts {} {} {}", data.drug_a, data.drug_b, data.severity);
+        let interaction_fact = format!(
+            "(interacts {} {} {}",
+            data.drug_a, data.drug_b, data.severity
+        );
 
         let req = UploadRequest::new()
             .namespace(PathBuf::from("fda/interactions"))
             .pattern("(interacts $a $b $s)".to_string())
-            .template("(__root__ (fda (interactions (__interactionsdata__ (interacts $a $b $s)))))".to_string())
+            .template(
+                "(__root__ (fda (interactions (__interactionsdata__ (interacts $a $b $s)))))"
+                    .to_string(),
+            )
             .data(interaction_fact);
 
         self.client.dispatch(req).await.map_err(|e| e.to_string());
@@ -34,11 +41,64 @@ impl GraphService {
         let req = UploadRequest::new()
             .namespace(namespace_path.clone())
             .pattern("(takes $u $d)".to_string())
-            .template(format!("(__root__(patients ({} (__{}data__ (takes $u $d)))))", data.user_id, data.user_id))
+            .template(format!(
+                "(__root__(patients ({} (__{}data__ (takes $u $d)))))",
+                data.user_id, data.user_id
+            ))
             .data(med_fact);
 
         self.client.dispatch(req).await.map_err(|e| e.to_string())
-        
     }
 
+    pub async fn check_risks(&self, user_id: String) -> Result<Vec<String>, String> {
+        let transform_input = TransformDetails::new()
+            .patterns(vec![
+                Mm2Cell::new_pattern(
+                    "(interacts $d1 $d2 $s)".to_string(),
+                    Namespace::from("fda/interactions"),
+                ),
+                Mm2Cell::new_pattern(
+                    format!("(takes {} $d1)", user_id),
+                    Namespace::from(format!("patients/{}", user_id)),
+                ),
+                Mm2Cell::new_pattern(
+                    format!("(takes {} $d2)", user_id),
+                    Namespace::from(format!("patients/{}", user_id)),
+                ),
+            ])
+            .templates(vec![
+                Mm2Cell::new_template("(Warning $d1 $d2 $s").to_string(), Namespace:;from("alerts")
+            ]);
+
+        // execute transformation
+        let _ = self
+            .client
+            .dispatch(TransformRequest::new().transform_input(transform_input))
+            .await;
+
+        // read Results
+        let read_input = TransformDetails::new()
+            .patterns(vec![Mm2Cell::new_pattern(
+                "$w".to_string(),
+                Namespace::from("alerts"),
+            )])
+            .templates(vec![Mm2Cell::new_template(
+                "$w".to_string(),
+                Namespace::from("alerts"),
+            )]);
+
+        let result = self
+            .client
+            .dispatch(ReadRequest::new().transform_input(read_input))
+            .await
+            .map_err(|e| e.to_string())?;
+
+        let findings: Vec<String> = result
+            .lines()
+            .filter(|line| line.contains("(Warning"))
+            .map(|s| s.trim().to_string())
+            .collect();
+
+        Ok(findings)
+    }
 }
